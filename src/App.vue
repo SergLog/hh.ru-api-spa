2368f39420978d19d41173147bcd254c7491c82b
Sergey Loginov <the.loginov@gmail.com>
2018-12-04 17:41:39 +0500

    Добавлен компонент testAPI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <div id="app">тест
+  <div id="app">
+<test-a-p-i></test-a-p-i>
+
   </div>
 </template>
 
 <script>
+
+import TestAPI from './components/TestAPI.vue'
+
 export default {
+  
   name: 'app',
+  components: { TestAPI },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
---
+++ src/components/testAPI.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      {{ Object.keys(employers).length }}
+{{ employers }}
+<!-- <b-btn @click="employers">cvcc</b-btn>  -->
+  </div>
+</template>
+
+<script>
+ import axios from 'axios'
+
+  export default {
+  name: 'TestAPI',
+  
+  data() {
+    return {
+      employers: {}      
+    }
+  },
+  
+  created() {
+    axios.get(`https://api.hh.ru/vacancies/?text=vue&area=1&per_page=100`)
+    .then(response => {
+      this.employers = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+
+    
+  }
+}
+  
+</script>
+
+
+<style scoped>
+</style>
